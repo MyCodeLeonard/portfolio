951a205e872dda9bd0ff1f90fb416e5cf1730d59
MyCodeLeonard <203679381+MyCodeLeonard@users.noreply.github.com>
2025-11-19 17:26:57 +0100

    feat: added portfolio section (a suitable image still needs to be added and placeholders are currently in use) | fix: marquee adjusted a little

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -4,11 +4,13 @@ import { HeaderComponent } from "../shared/components/header/header.component";
 import { AboutMeComponent } from "../about-me/about-me.component";
 import { FooterComponent } from "../shared/components/footer/footer.component";
 import { SkillsComponent } from "../skills/skills.component";
+import { PortfolioComponent } from "../portfolio/portfolio.component";
+import { OverlayComponent } from "../portfolio/overlay/overlay.component";
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [HeroComponent, HeaderComponent, AboutMeComponent, FooterComponent, SkillsComponent],
+  imports: [HeroComponent, HeaderComponent, AboutMeComponent, FooterComponent, SkillsComponent, PortfolioComponent, OverlayComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
