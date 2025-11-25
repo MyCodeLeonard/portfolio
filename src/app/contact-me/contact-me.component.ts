39e1ae225af29da212715c860f6fd7a000e5b9ba
MyCodeLeonard <203679381+MyCodeLeonard@users.noreply.github.com>
2025-11-25 21:23:17 +0100

    fix: Linked buttons to sections |  minor adjustments

--- src/app/contact-me/contact-me.component.ts
+++ src/app/contact-me/contact-me.component.ts
@@ -9,7 +9,18 @@ import { ClickButtonComponent } from "./click-button/click-button.component";
   styleUrl: './contact-me.component.scss'
 })
 export class ContactMeComponent {
-  test(){
-    console.log('test');
+
+  setFocus(input: HTMLInputElement) {
+    input.focus();
+  }
+
+  highlight(prevHr: HTMLHRElement, nextHr: HTMLHRElement) {
+    if (prevHr) prevHr.style.borderColor = '#3DCFB6';
+    if (nextHr) nextHr.style.borderColor = '#3DCFB6';
+  }
+
+  reset(prevHr: HTMLHRElement, nextHr: HTMLHRElement) {
+    if (prevHr) prevHr.attributeStyleMap.clear();
+    if (nextHr) nextHr.attributeStyleMap.clear();
   }
 }
