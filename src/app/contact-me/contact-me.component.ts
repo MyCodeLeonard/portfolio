a8fb86e59fe581fd280150b94b2a91d4ae26d9f5
MyCodeLeonard <203679381+MyCodeLeonard@users.noreply.github.com>
2025-12-09 00:57:00 +0100

    feat: Add logic for contact-me  and fix: apply adjustments to HTML, SCSS and translate-content.service.ts.

--- src/app/contact-me/contact-me.component.ts
+++ src/app/contact-me/contact-me.component.ts
@@ -1,29 +1,122 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject} from '@angular/core';
 import { ClickButtonComponent } from "../shared/components/click-button/click-button.component";
 import { TranslateContentService } from '../shared/translate-content.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-me',
   standalone: true,
-  imports: [ClickButtonComponent, CommonModule],
+  imports: [ClickButtonComponent, CommonModule, FormsModule],
   templateUrl: './contact-me.component.html',
   styleUrl: './contact-me.component.scss'
 })
 export class ContactMeComponent {
   translateContent = inject(TranslateContentService);
 
-  setFocus(input: HTMLInputElement) {
+  name!:string;
+  email!:string;
+  massage!:string;
+  privacyPolicy!:boolean;
+
+  isCheckName:boolean = true;
+  isCheckEmail!:boolean;
+  isCheckMassage!:boolean;
+  isCheckPrivacyPolicy:boolean = true;
+
+  yourNameError:string = this.translateContent[this.translateContent.language].yourNameError;
+  yourEmailError!:string;
+  helpyouError!:string;
+  privacyPolicyError:string = this.translateContent[this.translateContent.language].privacyPolicyError;
+
+  isButtonDisabled = true;
+  
+  senderFeedbackShow:boolean= false;
+
+  setFocus(input: HTMLElement) {
     input.focus();
   }
 
-  highlight(prevHr: HTMLHRElement, nextHr: HTMLHRElement) {
+  adjustHeight(textarea: any) {
+    textarea.style.height = '24px';
+    textarea.style.height = (textarea.scrollHeight) + 'px';
+  }
+
+  highlight(prevHr: HTMLElement, nextHr: HTMLElement) {
     if (prevHr) prevHr.style.borderColor = '#3DCFB6';
     if (nextHr) nextHr.style.borderColor = '#3DCFB6';
   }
 
-  reset(prevHr: HTMLHRElement, nextHr: HTMLHRElement) {
+  reset(prevHr: HTMLElement, nextHr: HTMLElement) {
     if (prevHr) prevHr.attributeStyleMap.clear();
     if (nextHr) nextHr.attributeStyleMap.clear();
+  }
+
+  buttonDisabled(){
+    if(this.isCheckName && this.isCheckEmail && this.isCheckMassage && this.isCheckPrivacyPolicy){
+      this.isButtonDisabled = false;
+    }
+    else{
+      this.isButtonDisabled = true;
+    }
+  }
+
+  formSend(){
+    this.checkName();
+    this.checkEmail();
+    this.checkMassage();
+    this.checkPrivacyPolicy();
+
+    if(this.isCheckName && this.isCheckEmail && this.isCheckMassage && this.isCheckPrivacyPolicy){
+      
+      this.senderFeedbackShow = true;
+      
+      this.name = '';
+      this.email = '';
+      this.massage = '';
+      this.privacyPolicy = false;
+
+      setTimeout(() => {
+        this.senderFeedbackShow = false;
+      }, 2000);
+    } 
+  }
+
+  checkName(){
+    if (!this.name?.trim()) return this.isCheckName = false;
+    else return this.isCheckName = true;
+  }
+
+  checkEmail(){
+    if (!this.email?.trim()) {
+      this.yourEmailError = this.translateContent[this.translateContent.language].yourEmailFirstError;
+      return this.isCheckEmail = false;
+    }
+    else if (!/^\S+@\S+\.\S+$/.test(this.email)){
+      this.yourEmailError = this.translateContent[this.translateContent.language].yourEmailSecondError; 
+      return this.isCheckEmail = false;
+    }
+    else{
+      return this.isCheckEmail = true;
+    } 
+  }
+
+  checkMassage(){
+    if (!this.massage?.trim()) {
+      this.helpyouError = this.translateContent[this.translateContent.language].helpyouFirstError;
+      return this.isCheckMassage = false;
+    }
+    else if(this.massage.length < 20){
+      this.helpyouError = this.translateContent[this.translateContent.language].helpyouSecondError;
+      return this.isCheckMassage = false
+    }
+    else{
+      return this.isCheckMassage = true;
+    } 
+  }
+
+  checkPrivacyPolicy(){
+    if(this.privacyPolicy) return this.isCheckPrivacyPolicy = true
+    else return this.isCheckPrivacyPolicy = false
   }
 }
