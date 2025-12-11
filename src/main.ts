a6ef293dc67f7e6ff91512df6054e3b15554a2bb
MyCodeLeonard <203679381+MyCodeLeonard@users.noreply.github.com>
2025-12-11 21:33:56 +0100

    fix: make minor adjustments

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
 
-const cursor = document.getElementById("cursor")
+const cursor = document.getElementById("cursor");
+
 document.addEventListener("mousemove", (e) => {
   cursor!.style.left = e.clientX + "px";
   cursor!.style.top = e.clientY + "px";
