cd8e6a78d9d635ef290276f6ae30ea969cc69c01
MyCodeLeonard <203679381+MyCodeLeonard@users.noreply.github.com>
2025-12-11 21:22:04 +0100

    fix:made Legal Notice and Privacy Policy responsive

--- src/app/legal-notice-page/legal-notice-page.component.ts
+++ src/app/legal-notice-page/legal-notice-page.component.ts
@@ -1,15 +1,15 @@
 import { Component, inject } from '@angular/core';
-import { FooterComponent } from "../shared/components/footer/footer.component";
-import { HeaderComponent } from "../shared/components/header/header.component";
 import { LegalNoticeEnglishComponent } from "./legal-notice-english/legal-notice-english.component";
 import { LegalNoticeGermanComponent } from "./legal-notice-german/legal-notice-german.component";
 import { TranslateContentService } from '../shared/translate-content.service';
 import { CommonModule } from '@angular/common';
+import { HeaderPageComponent } from "../shared/components/header-page/header-page.component";
+import { FooterPageComponent } from "../shared/components/footer-page/footer-page.component";
 
 @Component({
   selector: 'app-legal-notice-page',
   standalone: true,
-  imports: [FooterComponent, HeaderComponent, LegalNoticeEnglishComponent, LegalNoticeGermanComponent, CommonModule],
+  imports: [ LegalNoticeEnglishComponent, LegalNoticeGermanComponent, CommonModule, HeaderPageComponent, FooterPageComponent],
   templateUrl: './legal-notice-page.component.html',
   styleUrl: './legal-notice-page.component.scss'
 })
--- src/app/privacy-policy-page/privacy-policy-page.component.ts
+++ src/app/privacy-policy-page/privacy-policy-page.component.ts
@@ -1,15 +1,15 @@
 import { Component, inject } from '@angular/core';
-import { HeaderComponent } from "../shared/components/header/header.component";
-import { FooterComponent } from "../shared/components/footer/footer.component";
 import { PrivacyPolicyEnglishComponent } from "./privacy-policy-english/privacy-policy-english.component";
 import { PrivacyPolicyGermanComponent } from "./privacy-policy-german/privacy-policy-german.component";
 import { CommonModule } from '@angular/common';
 import { TranslateContentService } from '../shared/translate-content.service';
+import { HeaderPageComponent } from '../shared/components/header-page/header-page.component';
+import { FooterPageComponent } from "../shared/components/footer-page/footer-page.component";
 
 @Component({
   selector: 'app-privacy-policy-page',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, PrivacyPolicyEnglishComponent, PrivacyPolicyGermanComponent, CommonModule],
+  imports: [HeaderPageComponent, PrivacyPolicyEnglishComponent, PrivacyPolicyGermanComponent, CommonModule, FooterPageComponent],
   templateUrl: './privacy-policy-page.component.html',
   styleUrl: './privacy-policy-page.component.scss'
 })
