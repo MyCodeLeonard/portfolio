de040bdf37d679ef0a98b21f45e1329091da38d2
MyCodeLeonard <203679381+MyCodeLeonard@users.noreply.github.com>
2025-12-16 19:24:07 +0100

    fix:made special-icon responsive is not finish yet

--- src/app/main-page/skills/skill-set-icon/special-icon/special-icon.component.ts
+++ src/app/main-page/skills/skill-set-icon/special-icon/special-icon.component.ts
@@ -1,14 +1,49 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateContentService } from '../../../../shared/translate-content.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-special-icon',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule ],
   templateUrl: './special-icon.component.html',
   styleUrl: './special-icon.component.scss'
 })
 export class SpecialIconComponent {
-  translateContent = inject(TranslateContentService)
+  // @HostListener('window:resize') onResize() { 
+  //   this.customizationInfoBox();
+  // }
+
+  translateContent = inject(TranslateContentService);
+  
+  // @ViewChild('infoBox') infoBoxDivRef!:ElementRef<HTMLElement>
+  // infoBoxDiv!:HTMLElement;
+
+  // ngAfterViewInit() {
+  //   this.infoBoxDiv = this.infoBoxDivRef.nativeElement;
+  //   this.customizationInfoBox();
+  // }
+
+  // customizationInfoBox(){
+  //   let left = this.infoBoxDiv.getBoundingClientRect().left;
+  //   let right = this.infoBoxDiv.getBoundingClientRect().right;
+
+  //   this.infoBoxDiv?.style.removeProperty('margin-left');
+  //   this.infoBoxDiv?.style.removeProperty('margin-right');
+
+  //   // console.log('right');
+  //   // console.log(right);
+  //   // console.log('left');
+  //   // console.log(left);
+  //   // console.log('window.innerWidth');
+  //   // console.log(window.innerWidth);
+    
+  //   if(right >= window.innerWidth){
+  //     this.infoBoxDiv.style.marginRight = '160px';
+  //   } 
+  //   else if(left <= 0){
+  //     this.infoBoxDiv.style.marginLeft = '160px';
+  //   }
+  // }
 }
