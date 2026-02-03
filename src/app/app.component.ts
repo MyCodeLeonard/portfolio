7d238d0441458ab794270add7cbfcac4df9f1f70
MyCodeLeonard <203679381+MyCodeLeonard@users.noreply.github.com>
2026-02-03 07:04:20 +0100

    feat: add Animate On Scroll (AOS)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,11 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+
 
 @Component({
   selector: 'app-root',
@@ -9,6 +14,31 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'portfolio';
+
+  constructor(private router: Router) {}
+
+  ngAfterViewInit(): void {
+    // Initiales AOS init
+    this.initAOS();
+
+    // Bei jeder Navigation neu initialisieren
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.initAOS();
+      }
+    });
+  }
+
+  private initAOS() {
+    if (typeof AOS !== 'undefined') {
+      AOS.init({
+        duration: 1200,
+        once: false,
+        mirror: true
+      });
+      AOS.refresh(); // WICHTIG: damit neue Inhalte erkannt werden
+    }
+  }
 }
