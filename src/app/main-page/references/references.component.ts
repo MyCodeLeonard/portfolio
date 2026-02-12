ae91a5a431bccc7137ca542a20b1b7afd1247a39
MyCodeLeonard <203679381+MyCodeLeonard@users.noreply.github.com>
2026-02-12 10:20:27 +0100

    fix: Adjustments from feedback (09.02.) (section references -> completely adapted)

--- src/app/main-page/references/references.component.ts
+++ src/app/main-page/references/references.component.ts
@@ -1,8 +1,6 @@
-import { Component, inject } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, inject, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateContentService } from '../../shared/translate-content.service';
-
-type ColleaguesValue = 'colleagueOne' | 'colleagueTwo' | 'colleagueThree';
 
 @Component({
   selector: 'app-references',
@@ -12,49 +10,84 @@ type ColleaguesValue = 'colleagueOne' | 'colleagueTwo' | 'colleagueThree';
   styleUrl: './references.component.scss'
 })
 
-export class ReferencesComponent {
+export class ReferencesComponent implements AfterViewInit{
+  @ViewChild('track') track!: ElementRef<HTMLDivElement>
+  @ViewChild('box') box!: ElementRef<HTMLDivElement>
+
   translateContent = inject(TranslateContentService);
+
+  items: ('colleagueOne'| 'colleagueTwo' | 'colleagueThree')[] = ['colleagueThree', 'colleagueOne', 'colleagueTwo', ];
+  loopItems: ('colleagueOne'| 'colleagueTwo' | 'colleagueThree')[] = [...this.items, ...this.items, ...this.items];
+
+  step = 0;
+  index = 3;
   currentReference: 1 | 2 | 3 = 1;
+
+  isTransitioning = false;
+
+  boxClick(i:number){
+    if(i == this.index) this.prev();
+    if(i == this.index +2) this.next();
+  }
   
-  boxLeft:ColleaguesValue = 'colleagueOne';
-  boxMiddle:ColleaguesValue = 'colleagueTwo';
-  boxRight:ColleaguesValue = 'colleagueThree';
+  next() {
+    if (this.isTransitioning) return;
+    this.onResize();
+    this.isTransitioning = true;
+    this.index++;
+    this.track.nativeElement.style.transform = `translateX(${(this.index -3) * this.step *-1}px)`;
 
-  previousReferences(){
-    this.previousPointHighlight();
-    this.previousSortingBoxes();
-  }
-
-  previousPointHighlight(){
-    this.currentReference--;
-    if(this.currentReference <1) this.currentReference = 3
-  }
-
-  previousSortingBoxes(){
-    const previous: Record<ColleaguesValue, [ColleaguesValue, ColleaguesValue, ColleaguesValue]> = {
-      'colleagueOne': ['colleagueThree', 'colleagueOne', 'colleagueTwo'],
-      'colleagueTwo': ['colleagueOne', 'colleagueTwo', 'colleagueThree'],
-      'colleagueThree': ['colleagueTwo', 'colleagueThree', 'colleagueOne']
-    };
-    [this.boxLeft, this.boxMiddle, this.boxRight] = previous[this.boxLeft];
-  }
-
-  nextReferences(){
-    this.nextPointHighlight();
-    this.nextSortingBoxes();    
-  }
-
-  nextPointHighlight(){
     this.currentReference++;
     if(this.currentReference >3) this.currentReference = 1
   }
+  
+  prev() {
+    if (this.isTransitioning) return;
+    this.onResize();
+    this.isTransitioning = true;
+    this.index--;
+    this.track.nativeElement.style.transform = `translateX(${(this.index -3) * this.step *-1}px)`;
 
-  nextSortingBoxes(){
-    const next: Record<ColleaguesValue, [ColleaguesValue, ColleaguesValue, ColleaguesValue]> = {
-      'colleagueOne': ['colleagueTwo', 'colleagueThree', 'colleagueOne'],
-      'colleagueTwo': ['colleagueThree', 'colleagueOne', 'colleagueTwo'],
-      'colleagueThree': ['colleagueOne', 'colleagueTwo', 'colleagueThree']
-    };
-    [this.boxLeft, this.boxMiddle, this.boxRight] = next[this.boxLeft];
+    this.currentReference--;
+    if(this.currentReference <1) this.currentReference = 3
+  }
+  
+  onTransitionEnd() {
+    this.isTransitioning = false;
+    this.endReached();
+    this.beginningReached();
+  }
+
+  endReached(){
+    if (this.index >= this.items.length + 3) {
+      this.track.nativeElement.style.transition = 'none';
+      this.index = 3;
+      this.track.nativeElement.style.transform = `translateX(0)`;
+      setTimeout(() => this.track.nativeElement.style.removeProperty('transition'));
+    }
+  }
+
+  beginningReached(){
+    if (this.index <= 0) {
+      this.track.nativeElement.style.transition = 'none';
+      this.index = 3;
+      this.track.nativeElement.style.transform = `translateX(0)`;
+      setTimeout(() => this.track.nativeElement.style.removeProperty('transition'));
+    }
+  }
+
+  ngAfterViewInit(){
+    this.track.nativeElement.style.transform = `translateX(0)`;
+    this.onResize();
+  }
+  
+  @HostListener('window:resize')
+  onResize() {
+    if (!this.track.nativeElement || !this.box.nativeElement) return;
+    const widthBox = this.box.nativeElement.getBoundingClientRect().width;
+    const betweenBoxGap = parseFloat(window.getComputedStyle(this.track.nativeElement).gap);
+    
+    this.step = widthBox + betweenBoxGap;
+    this.track.nativeElement.style.transform = `translateX(${(this.index -3) * this.step *-1}px)`;
   }
 }
